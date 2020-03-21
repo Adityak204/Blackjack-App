fa89e88f0cfa8dd572bfeb385163c0d3bee1183c
Aditya Kumar Singh <adityak204@gmail.com>
2020-03-21 22:12:57 +0530

    Added comment for Deck class

--- Blackjack_myv.py
+++ Blackjack_myv.py
@@ -4,7 +4,7 @@ import random
 def input_val():
     name = input('Enter you name')
     return name
-
+#Deck is a class, defining features inside it
 class Deck:
     def __init__(self):
         card_face = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
